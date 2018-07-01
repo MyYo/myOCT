96269069f31c80e9680e471d183b78d4b39f0dd0
MyYo <MyYo@users.noreply.github.com>
2018-06-30 21:35:08 -0700

    [Minor] Modified Demo_3D to work in a batch mode

--- Demo_3D.m
+++ Demo_3D.m
@@ -1,46 +1,69 @@
-%Run this demo to load and process 3D OCT Images
+%Run this demo to load and process 3D OCT Images in a batch format due to
+%memory limitations
 
 %% Iputs
 
 %Ganymede
 %filePath = 's3://delazerdalab2/CodePackage/TestVectors/Ganymede3D_BScanAvg/';
 %OCTSystem = 'Ganymede';
-%YFramesToProcess = 1:10:500;
 %dispersionParameterA = 0.0058; %Use this dispersion Parameter for air-water interface
 
 %Wasatch
 filePath = 's3://delazerdalab2/CodePackage/TestVectors/Wasatch3D/';
 OCTSystem = 'Wasatch';
-YFramesToProcess = 1:2;
 dispersionParameterA = 2.271e-02; %Use this dispersion Parameter for air-water interface
 
+yFramesPerBatch = 10; %How many Y frames to load in a single batch, 
+                      %optimzie this parameter to save computational time
+
+%% Gather Header Data
+dimensions = yOCTLoadInterfFromFile(filePath, 'OCTSystem', OCTSystem, 'PeakOnly', true);
+ys = dimensions.y.index;
+
+yStart = ys(1:yFramesPerBatch:(end-1));
+yEnd   = ys([yFramesPerBatch:yFramesPerBatch:end end]);
+
+%Stucture containing the output of the process (z,x,y)
+outputMat = zeros(...
+    length(dimensions.lambda.values)/2,length(dimensions.x.values),length(dimensions.y.values), ...
+    'single');
 
 %% Process
 tic;
-
-%Load Intef From file
-[interf,dimensions] = yOCTLoadInterfFromFile(filePath,'OCTSystem',OCTSystem ...
-    ,'BScanAvgFramesToProcess', 1:2 ... To save time, load first few BScans. Comment out this line to load all BScans
-    ,'YFramesToProcess',YFramesToProcess ... To save time, don't load all frames along y direction. Comment out this line to load all frames
+for yi=1:length(yStart) %Loop for each batch
+    y = yStart(yi):yEnd(yi);
+    
+    %% Load and Preprocess data
+    
+    %Load Intef From file
+    [interf,dimensionsItr] = yOCTLoadInterfFromFile(filePath,'OCTSystem',OCTSystem ...
+        ,'YFramesToProcess',y ... To save time, don't load all frames along y direction. Comment out this line to load all frames
     );
 
-%Generate BScans
-scanCpx = yOCTInterfToScanCpx(interf,dimensions ...
-    ,'dispersionParameterA', dispersionParameterA ...
-    );
+    %Generate BScans
+    scanCpx = yOCTInterfToScanCpx(interf,dimensionsItr ...
+        ,'dispersionParameterA', dispersionParameterA ...
+        );
+    
+    %% Add processing code here, process your frame as you would like
 
-%Average along BScanAvg dimension
-if isfield(dimensions,'BSCanAvg')
-    scanAbs = mean(abs(scanCpx),dimensions.BScanAvg.order);
-else
-    scanAbs = abs(scanCpx);
+    %Example:
+    %Average along BScanAvg dimension
+    if isfield(dimensionsItr,'BSCanAvg')
+        scanAbs = mean(abs(scanCpx),dimensionsItr.BScanAvg.order);
+    else
+        scanAbs = abs(scanCpx);
+    end
+    
+    %% Save processed result to main matrix
+    outputMat(:,:,y) = scanAbs;
 end
-
 toc;
-%% Visualization
+
+%% Visualization - example
 %Preform max projection along z
 figure(1);
-scanAbsMxProj = squeeze(max(scanAbs,[],dimensions.lambda.order));
+scanAbsMxProj = squeeze(max(outputMat,[],dimensions.lambda.order));
 imagesc(dimensions.x.values, dimensions.y.values, scanAbsMxProj');
 xlabel(['x direction [' dimensions.x.units ']']);
 ylabel(['y direction [' dimensions.y.units ']']);
