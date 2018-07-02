849fa4d87cdac61b85e188105c0deeb3ab6a909c
MyYo <MyYo@users.noreply.github.com>
2018-07-02 15:23:44 -0700

    [Minor] Made parameters more visible

--- Demo_Spectral.m
+++ Demo_Spectral.m
@@ -1,4 +1,5 @@
 %This demo demonstrates how to extract images from 2 bands (spectral OCT)
+%this demo is for 2D but can be extanded for 3D
 
 %% Iputs
 %Ganymede
@@ -6,13 +7,14 @@ filePath = 's3://delazerdalab2/CodePackage/TestVectors/Ganymede2D_BScanAvg/';
 OCTSystem = 'Ganymede';
 band1 = [800 900]; %[nm]
 band2 = [900 1000]; %[nm]
+dispersionParameterA = 0.0058; %Use this dispersion Parameter for air-water interface
 
 %% Process
 tic;
 
 %Load Intef From file
 [interf,dimensions] = yOCTLoadInterfFromFile(filePath,'OCTSystem',OCTSystem ...
-    ,'BScanAvgFramesToProcess', 1:2 ... To save time, load first few BScans. Comment out this line to load all BScans
+    ,'BScanAvgFramesToProcess', 1 ... To save time, load first few BScans. Comment out this line to load all BScans
     );
 
 %Equispace interferogram to save computation time while re-processing
@@ -20,12 +22,12 @@ tic;
 
 %Generate BScans - Band1
 scanCpx1 = yOCTInterfToScanCpx(interf,dimensions ...
-    ,'dispersionParameterA', 0.0058 ...Use this dispersion Parameter for air-water interface
+    ,'dispersionParameterA', dispersionParameterA ...Use this dispersion Parameter for air-water interface
     ,'band',band1);
 
 %Generate BScans - Band2
 scanCpx2 = yOCTInterfToScanCpx(interf,dimensions ...
-    ,'dispersionParameterA', 0.0058 ...Use this dispersion Parameter for air-water interface
+    ,'dispersionParameterA', dispersionParameterA ...Use this dispersion Parameter for air-water interface
     ,'band',band2);
 
 toc;
