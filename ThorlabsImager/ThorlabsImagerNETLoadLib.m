54f26e8be96fc148dc4878fa7ad64e8aade3ad0d
MyYo <MyYo@users.noreply.github.com>
2019-07-30 14:04:32 -0700

    .Net library load only once

--- ThorlabsImager/ThorlabsImagerNETLoadLib.m
+++ ThorlabsImager/ThorlabsImagerNETLoadLib.m
@@ -1,18 +1,18 @@
 function ThorlabsImagerNETLoadLib()
 %This function loads the library
 
-global ThorlabsImagerNETLoaded
-if isempty(ThorlabsImagerNETLoaded) || ~ThorlabsImagerNETLoaded
+if isempty(which('ThorlabsImagerNET.ThorlabsImager')) %If the dll was not loaded
 
     currentFileFolder = fileparts(mfilename('fullpath'));
 	libFolder = [currentFileFolder '\Lib\'];
 	
-    % Copy Subfolders to here
-	copyfile([libFolder 'LaserDiode\*.*'],libFolder,'f');
-	copyfile([libFolder 'MotorController\*.*'],libFolder,'f');
-	copyfile([libFolder 'ThorlabsOCT\*.*'],libFolder,'f');
+	if ~exist([libFolder 'SpectralRadar.dll'],'file')
+		% Copy Subfolders to main lib folder
+		copyfile([libFolder 'LaserDiode\*.*'],libFolder,'f');
+		copyfile([libFolder 'MotorController\*.*'],libFolder,'f');
+		copyfile([libFolder 'ThorlabsOCT\*.*'],libFolder,'f');
+	end
     
     %Load Assembly
     asm = NET.addAssembly([libFolder 'ThorlabsImagerNET.dll']);
-    ThorlabsImagerNETLoaded = true;
 end
