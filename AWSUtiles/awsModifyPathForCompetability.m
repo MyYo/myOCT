c1485468576e568c2baf0e20e71d6b4e32876706
MyYo <MyYo@users.noreply.github.com>
2019-08-10 18:04:33 -0700

    support for non AWS paths

--- AWSUtiles/awsModifyPathForCompetability.m
+++ AWSUtiles/awsModifyPathForCompetability.m
@@ -1,23 +1,30 @@
 function p = awsModifyPathForCompetability (p,isAWS_CLI)
 %This function excepts path and output AWS compatible path.
+%If p is not AWS path will modify it to be compatible with windows
 %INPUTS: 
 %   p - path
 %   isAWS_CLI - will this path be used with AWS CLI? if so, spaces are
 %   defined slightly differently. If used path for datastore, set to false
 
-if ~exist('isAWS_CLI','var')
-    isAWS_CLI = false;
-end
+if (awsIsAWSPath(p)
+	if ~exist('isAWS_CLI','var')
+		isAWS_CLI = false;
+	end
 
-p = strrep(p,'\','/'); %Repalce slashes
+	p = strrep(p,'\','/'); %Repalce slashes
 
-%Replace double slashed
-p = strrep(p,'//','/'); 
-p = strrep(p,'//','/'); 
-p = strrep(p,'s3:/','s3://'); 
+	%Replace double slashed
+	p = strrep(p,'//','/'); 
+	p = strrep(p,'//','/'); 
+	p = strrep(p,'s3:/','s3://'); 
 
-if isAWS_CLI
-    p = strrep(p,'%20',' '); %Replace Spaces
+	if isAWS_CLI
+		p = strrep(p,'%20',' '); %Replace Spaces
+	else
+		p = strrep(p,' ','%20'); %Replace Spaces
+	end
 else
-    p = strrep(p,' ','%20'); %Replace Spaces
+	%Regular path
+	p = strrep(p,'/','\'); %Repalce slashes
+	p = strrep(p,'\\','\'); %Replace double slashed
 end
