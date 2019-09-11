df9c89bcd46176ba1354586afd466e81ab732ea8
MyYo <MyYo@users.noreply.github.com>
2019-09-11 15:51:44 -0700

    Documentation changes

--- ThorlabsImager/DemoTiledScan.m
+++ ThorlabsImager/DemoTiledScan.m
@@ -2,13 +2,13 @@
 
 %% Scan
 yOCTScanTile(...
-    'MyScan\',... Output folder
+    'D:\Yonatan\s1\',... Output folder
     ... Optional parameters, see documentation for full list of parameters
     'octProbePath','C:\Program Files\Thorlabs\SpectralRadar\Config\Probe - Olympus 10x.ini',...
     'isVerifyMotionRange',false, ...
     'nXPixels',200, ... %Number of pixels along the x direction
-    'nYPixels',10,  ... %Number of pixels along the y direction
-    'xToScan' ,[-0.5 0.5], ...[mm] center of scan
-    'yToScan' ,[-0.5 0.5], ...[mm] center of scan
-    'zToScan', [0 0.1] ...[mm]
+    'nYPixels',200,  ... %Number of pixels along the y direction
+    'xCenters' ,[0 1], ...[mm] center of scan
+    'yCenters' ,[0 1], ...[mm] center of scan
+    'zDepts', [0 0.1] ...[mm]
 );
