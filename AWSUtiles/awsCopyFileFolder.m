1edb6200341fc11b17cedf9da7ba74619f451b36
MyYo <MyYo@users.noreply.github.com>
2019-10-16 22:05:25 -0700

    Allow awsCopyFileFolder to copy files locally if required

--- AWSUtiles/awsCopyFileFolder.m
+++ AWSUtiles/awsCopyFileFolder.m
@@ -25,9 +25,10 @@ else
     isDestAWS = false;
 end
 
-%% Figure out what is been done
-mode = '';
-if ~isSourceAWS
+%% Upload Mode
+if ~isSourceAWS && isDestAWS
+    %Figure out what is been done
+    mode = '';
     if exist(source,'dir')
         mode = 'UploadDir';
         
@@ -50,41 +51,57 @@ if ~isSourceAWS
     else
         error('Cannot find: %s',source);
     end
+    
+    %Preform the upload
+    txt = [];
+    switch(mode)
+        case 'UploadFile'
+            [err,txt] = system(['aws s3 cp "' source '" "' dest '"']);
+        case {'UploadDir','UploadDirManySmallFiles'}
+            if (v)
+                fprintf('Uploading %.1f GBytes, %d files...\n',totalDataTransferVolume,numberOfFiles);
+                tt = tic();
+            end
+
+            if strcmp(mode,'UploadDir')
+                [err,txt] = system(['aws s3 sync "' source '" "' dest '"']);
+            elseif strcmp(mode,'UploadDirManySmallFiles')
+                err = 0;
+                awsCopyFileFolder_ManySmallFiles(source,dest,v);
+            end
+
+            if (v)
+                tt = toc(tt);
+                fprintf(['Total Time %.0f[min].\n' ...
+                    'Overall average upload speed: \n' ...
+                    '  %.1f [sec] to send one Gigabyte\n' ...
+                    '  %.1f [Mbytes/sec]\n'],tt/60,...
+                    tt/totalDataTransferVolume,totalDataTransferVolume*1024/tt);
+            end
+        otherwise
+            error('Couldn''t figure out the mode of operation');
+    end
+
+    if err~=0
+        error(['error happend while using aws: ' txt]);
+    end
+
+%% Copy local file mode
+elseif (~isSourceAWS && ~isDestAWS)
+    
+    if (isfile(source))
+        %File copy
+        copyfile(source,dest);
+    else
+        %Folder copy
+        system(['xcopy "' source '" "' dest '" /S /C /Q /Y']);
+    end
+    
+%% Other
+else
+    error('Don''t know how to isSourceAWS: %d, isDestAWS: %d',isSourceAWS,isDestAWS)
 end
 
-%% Preform the upload
-txt = [];
-switch(mode)
-    case 'UploadFile'
-        [err,txt] = system(['aws s3 cp "' source '" "' dest '"']);
-    case {'UploadDir','UploadDirManySmallFiles'}
-		if (v)
-            fprintf('Uploading %.1f GBytes, %d files...\n',totalDataTransferVolume,numberOfFiles);
-            tt = tic();
-        end
-		
-		if strcmp(mode,'UploadDir')
-			[err,txt] = system(['aws s3 sync "' source '" "' dest '"']);
-		elseif strcmp(mode,'UploadDirManySmallFiles')
-			err = 0;
-			awsCopyFileFolder_ManySmallFiles(source,dest,v);
-		end
-		
-		if (v)
-			tt = toc(tt);
-			fprintf(['Total Time %.0f[min].\n' ...
-                'Overall average upload speed: \n' ...
-                '  %.1f [sec] to send one Gigabyte\n' ...
-                '  %.1f [Mbytes/sec]\n'],tt/60,...
-                tt/totalDataTransferVolume,totalDataTransferVolume*1024/tt);
-		end
-    otherwise
-        error('Couldn''t figure out the mode of operation');
-end
-
-if err~=0
-    error(['error happend while using aws: ' txt]);
-end
 
 %% Fast copy of many small files
 function awsCopyFileFolder_ManySmallFiles(localSource,s3Dest,v)
