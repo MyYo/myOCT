d1ecef22399629888e08d54c2f2d3b68d86c8ace
MyYo <MyYo@users.noreply.github.com>
2020-01-23 18:16:24 -0800

    Add additional information to resliced volume

--- Processing/yOCTReslice.m
+++ Processing/yOCTReslice.m
@@ -26,6 +26,9 @@ function [reslicedVolume,xyzNew2Original,dimensions_n] = yOCTReslice(varargin)
 %       + a path to json file containing dimensions structure
 %       + empty or not specified, will try to retreave dimensions from meta
 %           data associated with volume file
+%   - appendToDimensions - a data structure to append to dimensions
+%       structure, in case additional information is required to be
+%       saved with the dimensions strucutre.
 %   - outputFileOrFolder - if specified, will not return reslicedVolume
 %       but would save it directly to hard drive (or s3). Use this option
 %       if running on the cloud as it will save the data transfer back and
@@ -55,6 +58,7 @@ addRequired(p,'y1_n',@(w)(all(w==sort(w))));
 addRequired(p,'z1_n',@(w)(all(w==sort(w))));
 
 addParameter(p,'dimensions',[])
+addParameter(p,'appendToDimensions',[]);
 addParameter(p,'outputFileOrFolder','');
 addParameter(p,'clearOutputFileOrFolderIfExists',true);
 addParameter(p,'verbose',false);
@@ -148,6 +152,9 @@ elseif isnumeric(in.xyzNew2Original) && numel(in.xyzNew2Original) == 3
     % Rotation matrix
     M_n2o = [mx my mz];
     
+    % Save transform to file
+    appendToDimensions.new2OriginalAffineTransform = [M_n2o [0 ; 0; 0]; [0 0 0 1]]; %No translation
+    
     xyzNew2Original = @(x,y,z)(M_n2o*[x(:)' ; y(:)' ; z(:)']);
 else 
     error('can''t parse xyzNew2Original');
@@ -166,6 +173,13 @@ dimensions_n.y.order = 3;
 dimensions_n.y.values = y1_n;
 dimensions_n.y.index = 1:length(y1_n);
 dimensions_n.y.units = 'mm';
+
+%Add append structured data (if needed)
+if ~isempty(in.appendToDimensions)
+    for fn = fieldnames(in.appendToDimensions)'
+        dimensions_n.(fn{1}) = appendToDimensions.(fn{1});
+    end
+end
 
 %% Compute output volume
 yOCT2Tif([],outputFileOrFolder,'partialFileMode',1);
