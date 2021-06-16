d4bf8b18a5f23d5cc5495d5bef4881e1e0a98f98
MyYo <MyYo@users.noreply.github.com>
2021-06-16 00:12:22 -0700

    Added more documentation about converting stage units to mm, and recalibrated translation stage

--- ThorlabsImager/ThorlabsImagerDll/ThorlabsImagerStage.cpp
+++ ThorlabsImager/ThorlabsImagerDll/ThorlabsImagerStage.cpp
@@ -7,7 +7,7 @@
 #include <math.h>
 using namespace std;
 
-int getSNByAxes(char axes) //Run Thorlabs.MotionControl.Kinesis.exe to see which serial numbers are avilable
+long getSNByAxes(char axes) //Run Thorlabs.MotionControl.Kinesis.exe to see which serial numbers are avilable
 {
 	switch (axes)
 	{
@@ -25,41 +25,57 @@ int getSNByAxes(char axes) //Run Thorlabs.MotionControl.Kinesis.exe to see which
 	}
 }
 
+// Return how much is one mm in device units
+double mmToDeviceUnits(long sn) //Stage SN
+{
+	// To get those numbers we move can move 100000 device points, and read what was the movement in mm
+	// or use Kinesis software to move known mm and see how much of units it came to be
+	// A more accurate design is by using Stage Calibration scripts in 00 Calibration and Analysis Scripts (OCT-Hist)
+
+	switch (sn)
+	{
+	case 27254221:
+		return 34609.935;
+	case 27254232:
+		return 34862.567;
+	default:
+		return 100000 / 2.9151;
+	}
+}
+
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////
 //Initialize Stage
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////
-double yOCTStageInit(char axes)
+double yOCTStageInit(char axes) // returns current position in mm
 {
-
-	double unitconversion = 2.9151 / 100000;
 
 	if (TLI_BuildDeviceList() == 0)
 	{
-		int serialNo = getSNByAxes(axes);
+		long serialNo = getSNByAxes(axes);
 
-		char testSerialNo[16];
-		sprintf_s(testSerialNo, "%d", serialNo);
+		char serialNoText[16];
+		sprintf_s(serialNoText, "%d", serialNo);
 
 		// operate device
-		if (CC_Open(testSerialNo) == 0)
+		if (CC_Open(serialNoText) == 0)
 		{
 			// start the device polling at 200ms intervals
-			CC_StartPolling(testSerialNo, 200);
+			CC_StartPolling(serialNoText, 200);
 
 			Sleep(3000);
-			CC_ClearMessageQueue(testSerialNo);
+			CC_ClearMessageQueue(serialNoText);
 
 			// get actual poaition
-			double pos = CC_GetPosition(testSerialNo);
-			pos = pos * unitconversion;
-			
+			double pos = CC_GetPosition(serialNoText);
+			pos = pos / mmToDeviceUnits(serialNo); // Convert pos to mm
+
 			// set velocity & acc if desired
 			int velocity = 1000000; //Units unknown. 0 means no limit. max vilocity 
-			int acc = 400; //Units unknown
+			int acc = 40; //Units unknown (default is 400)
 			int currentMaxVelocity, currentAcceleration;
-			CC_GetVelParams(testSerialNo, &currentAcceleration, &currentMaxVelocity);
-			printf("Device: %s current Acc: %d, max Vel: %d\n", testSerialNo, currentAcceleration, currentMaxVelocity);
-			CC_SetVelParams(testSerialNo, acc, velocity);
+			CC_GetVelParams(serialNoText, &currentAcceleration, &currentMaxVelocity);
+			printf("Device: %s current Acc: %d, max Vel: %d\n", serialNoText, currentAcceleration, currentMaxVelocity);
+			CC_SetVelParams(serialNoText, acc, velocity);
 
 			return pos;
 		}
@@ -68,7 +84,7 @@ double yOCTStageInit(char axes)
 	}
 
 	return -1;
-	
+
 }
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -79,30 +95,29 @@ double yOCTStageInit(char axes)
 void yOCTStageSetPosition(char axes, double position)
 {
 
-	double unitconversion = 2.9151 / 100000; //Conversion between internal and external units, determined ampiricly
-	position = position / unitconversion;
-
+	// Get device
 	int serialNo = getSNByAxes(axes);
+	char serialNoText[16];
+	sprintf_s(serialNoText, "%d", serialNo);
 
-	char testSerialNo[16];
-	sprintf_s(testSerialNo, "%d", serialNo);
-
+	// Convert mm to device units 
+	position = position * mmToDeviceUnits(serialNo);
 
 	// move to position (channel 1)
-	CC_ClearMessageQueue(testSerialNo);
-	CC_MoveToPosition(testSerialNo, (int)position);
-	printf("Device %s moving\r\n", testSerialNo);
+	CC_ClearMessageQueue(serialNoText);
+	CC_MoveToPosition(serialNoText, (int)position);
+	printf("Device %s moving\r\n", serialNoText);
 
 	WORD messageType;
 	WORD messageId;
 	DWORD messageData;
 	// wait for completion
-	do 
+	do
 	{
 		//double pos = CC_GetPosition(testSerialNo);
 		//printf("Position %f\n", pos);
 		//Sleep(100);
-		CC_WaitForMessage(testSerialNo, &messageType, &messageId, &messageData);
+		CC_WaitForMessage(serialNoText, &messageType, &messageId, &messageData);
 	} while (messageType != 2 || messageId != 1);
 }
 
