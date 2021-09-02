e4aa8f05ff2e5d28de2d87d535c2cf8f5b5dc874
Jessica <jt658@stanford.edu>
2021-09-01 20:45:54 -0700

    Added initial code and psuedo-code/comments

--- Processing/yOCTFindFocus.m
+++ Processing/yOCTFindFocus.m
@@ -19,3 +19,72 @@ function zFocusPix = yOCTFindFocus(varargin)
 %                 case of tild scan, one zFocusPix will be given for each
 %                 scan.
 
+% Questions
+% 1. Do we need to factor in optical path correction here? Yes
+% 2. How does this function relate to the other findFocusInBScan function?
+%    Does the manual refinement relate to the user input section of the
+%    findFocusInBScan function? Where will the focus correction fit in?
+% 3. How should we structure the json focusPositionInImageZpix param? 
+% 4. What is the manualRefinement parameter for?
+% 5. What is the path for the input folder?
+
+%% Parse Inputs from varargin and load certain fields from json
+
+scanInfoJson = awsReadJSON([inputDataFolder 'ScanInfo.json']);
+dispersionQuadraticTerm = scanInfoJson.DefaultDispersionQuadraticTerm;
+optPathCorrection = scanInfoJson.OpticalPathCorrectionPolynomial;
+yFramesPerBatch = 1; %How many Y frames to load in a single batch, optimzie this parameter to save computational time
+
+
+%% If zDepthStitchingMode is set
+if zDepthStitchingMode
+    % Count the number of Data folders within the inputDataFolder
+    file = dir(intputDataFolder);
+    filenames = {file.name};
+    numDataFiles = sum(~cellfun(@isempty, strfind(filenames, 'Data')));
+
+    % Create zFocusPix array to store the focus of each volume
+    zFocusPix = zeros(1, numDataFiles);
+
+    % Iterate through all data folders in inputDataFolder and reconstruct each 
+    % volume which was acquired at a different depth. Optical Path Correction?
+    for ind = 1:numDataFiles
+        if ind < 10
+            filePath = [inputDataFolder, '/Data0' int2str(ind) '/'];
+        else
+            filePath = [inputDataFolder, '/Data' int2str(ind) '/'];
+        end
+        
+        % Process
+        [meanAbs,speckleVariance,dimensions] = yOCTProcessScan(filePath, ...
+            {'meanAbs','speckleVariance'}, ... Which functions would you like to process. Option exist for function hendel
+            'dispersionQuadraticTerm', dispersionQuadraticTerm, ...
+            'nYPerIteration', yFramesPerBatch, ...
+            'showStats',true);
+        
+        % Optical Path Correction
+        [meanAbsPathCorrected, meanAbsValidDataMap] = yOCTOpticalPathCorrection(meanAbs, dimensions, optPathCorrection);
+
+
+        % Use the reconstructed volume to find the focus and store it in zFocusPix
+        [max_val, focus] = max(mean(squeeze(log(meanAbsPathCorrected)), [2 3]));
+        zFocusPix(focusInd) = focus;
+        focusInd = focusInd + 1;
+    end
+%% Else if zDepthStitchingMode is NOT set repeat above process for just the specified OCT file
+else   
+    % Reconstruct the chosen volume. Optical Path Correction?
+    [meanAbs,speckleVariance,dimensions] = yOCTProcessScan(inputDataFolder, ...
+            {'meanAbs','speckleVariance'}, ... Which functions would you like to process. Option exist for function hendel
+            'dispersionQuadraticTerm', dispersionQuadraticTerm, ...
+            'nYPerIteration', yFramesPerBatch, ...
+            'showStats',true);
+        
+    % Optical Path Correction
+    scanInfoJson = awsReadJSON([inputDataFolder 'ScanInfo.json']);
+    [meanAbsPathCorrected, meanAbsValidDataMap] = yOCTOpticalPathCorrection(meanAbs, dimensions, optPathCorrection);
+
+    % Use the reconstructed volume to find the focus and store it in zFocusPix
+    [max_val, zFocusPix] = max(mean(squeeze(log(meanAbsPathCorrected)), [2 3]));
+end
+
