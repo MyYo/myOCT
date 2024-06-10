3de005270a6174c752e2263c0ac329ec4ead8a40
MyYo <MyYo@users.noreply.github.com>
2024-06-10 14:32:44 -0700

    Vertical and horizontal lines were flipped

--- Demo_PhotobleachHashtag.m
+++ Demo_PhotobleachHashtag.m
@@ -23,16 +23,16 @@ vLinePositions = [-4  0 1 3]; %Unitless, vLine positions as multiplication of ba
 hLinePositions = [-3 -2 1 3]; %Unitless, hLine positions as multiplication of base
 
 % Define lines from pattern - v
-x_start_mm = -lineLength/2*ones(size(vLinePositions));
-x_end_mm   = +lineLength/2*ones(size(vLinePositions));
-y_start_mm = vLinePositions*base+vLineBias;
-y_end_mm   = vLinePositions*base+vLineBias;
+x_start_mm = vLinePositions*base+vLineBias;
+x_end_mm   = vLinePositions*base+vLineBias;
+y_start_mm = -lineLength/2*ones(size(vLinePositions));
+y_end_mm   = +lineLength/2*ones(size(vLinePositions));
 
 % Define lines from pattern - h
-x_start_mm = [x_start_mm  hLinePositions*base+hLineBias];
-x_end_mm   = [x_end_mm    hLinePositions*base+hLineBias];
-y_start_mm = [y_start_mm -lineLength/2*ones(size(hLinePositions))];
-y_end_mm   = [y_end_mm   +lineLength/2*ones(size(hLinePositions))];
+x_start_mm = [x_start_mm -lineLength/2*ones(size(hLinePositions))];
+x_end_mm   = [x_end_mm   +lineLength/2*ones(size(hLinePositions))];
+y_start_mm = [y_start_mm  hLinePositions*base+hLineBias];
+y_end_mm   = [y_end_mm    hLinePositions*base+hLineBias];
 
 % Photobleach configurations
 exposure_mm_sec = 20; % mm/sec
